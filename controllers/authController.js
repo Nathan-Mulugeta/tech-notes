0a4fc1d6c58f304aa8501c63b9767bb5210a402b
Nathan Mulugeta <nathanmulugetacoding@gmail.com>
2023-12-01 22:15:38 +0300

    update the refresh token expiry time

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,7 @@ const login = asyncHandler(async (req, res) => {
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: '1m' }
+    { expiresIn: '15m' }
   );
 
   const refreshToken = jwt.sign(
@@ -82,7 +82,7 @@ const refresh = (req, res) => {
           },
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '1m' }
+        { expiresIn: '15m' }
       );
 
       res.json({ accessToken });
