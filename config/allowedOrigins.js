04418c8a3016b5160a48d391162e51046dbfad0c
Nathan Mulugeta <nathanmulugetacoding@gmail.com>
2023-12-02 11:55:31 +0300

    prepare for deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,3 @@
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://www.dandrepairshop.com',
-  'https://dandrepairshop.com',
-];
+const allowedOrigins = ['https://technotes-5yg5.onrender.com'];
 
 module.exports = allowedOrigins;
