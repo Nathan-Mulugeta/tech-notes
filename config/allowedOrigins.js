bb3735a66e1d9d64d2d4b37caf4b295bc139bbd1
Nathan Mulugeta <nathanmulugetacoding@gmail.com>
2023-12-03 19:14:13 +0300

    .

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['https://technotes-5yg5.onrender.com'];
+const allowedOrigins = [
+  'https://technotes-5yg5.onrender.com',
+  'http://localhost:5174/',
+];
 
 module.exports = allowedOrigins;
